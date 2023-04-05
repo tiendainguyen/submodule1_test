7a462ea7952c64ac26d5f7aaf2a78977e210246a
tiendainguyen <daint.dev@gmail.com>
2023-04-05 23:02:43 +0700

    first

---
+++ src/main/java/com/example/submodule1_test/Submodule1TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.submodule1_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Submodule1TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Submodule1TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/submodule1_test/Submodule1TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submodule1_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Submodule1TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
